b5091475cbec669dab9aea98ba832d0cd87fec52
Tapdig Guliyev <mac_005751@mac-005751.local>
2022-12-08 10:04:29 +0400

    Bottom navigation bar applied with navigation components, BUG: can't open moviesFragment when in movieDetailsFragment

--- app/src/main/java/az/abb/tap/cinephilia/application/MainActivity.kt
+++ app/src/main/java/az/abb/tap/cinephilia/application/MainActivity.kt
@@ -1,7 +1,10 @@
 package az.abb.tap.cinephilia.application
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import az.abb.tap.cinephilia.R
 import az.abb.tap.cinephilia.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,5 +16,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/az/abb/tap/cinephilia/feature/feature1/view/MoviesFragment.kt
+++ app/src/main/java/az/abb/tap/cinephilia/feature/feature1/view/MoviesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import az.abb.tap.cinephilia.R
 import az.abb.tap.cinephilia.databinding.FragmentMoviesBinding
 
 class MoviesFragment : Fragment() {
@@ -13,5 +15,13 @@ class MoviesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentMoviesBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvName.setOnClickListener {
+            findNavController().navigate(R.id.action_moviesFragment_to_movieDetailsFragment)
+        }
     }
 }
