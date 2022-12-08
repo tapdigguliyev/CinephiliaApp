446834579a67c1a6d29e70d75ac06ed0f9b33f9e
Tapdig Guliyev <mac_005751@mac-005751.local>
2022-12-08 11:07:28 +0400

    RecylerViews added for movies and top rated movies

--- app/src/main/java/az/abb/tap/cinephilia/feature/feature1/view/MoviesFragment.kt
+++ app/src/main/java/az/abb/tap/cinephilia/feature/feature1/view/MoviesFragment.kt
@@ -6,11 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import az.abb.tap.cinephilia.R
+import az.abb.tap.cinephilia.base.BaseAdapter
 import az.abb.tap.cinephilia.databinding.FragmentMoviesBinding
+import az.abb.tap.cinephilia.databinding.ItemMediaBinding
 
 class MoviesFragment : Fragment() {
     private lateinit var binding: FragmentMoviesBinding
+    private val topRatedMoviesAdapter by lazy { BaseAdapter<String>() }
+    private val moviesAdapter by lazy { BaseAdapter<String>() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentMoviesBinding.inflate(layoutInflater, container, false)
@@ -19,9 +24,54 @@ class MoviesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupTopRatedMoviesRecylerView()
+        setupMoviesRecyclerView()
+        setupTopRatedMoviesAdapter()
+        setupMoviesAdapter()
 
         binding.tvName.setOnClickListener {
             findNavController().navigate(R.id.action_moviesFragment_to_movieDetailsFragment)
+        }
+    }
+
+    private fun setupTopRatedMoviesAdapter() {
+        topRatedMoviesAdapter.expressionOnCreateViewHolder = { viewGroup ->
+            ItemMediaBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        }
+
+        topRatedMoviesAdapter.expressionOnBindViewHolder = { topRatedMovie, viewBinding ->
+            val view = viewBinding as ItemMediaBinding
+            view.root.setOnClickListener {
+                findNavController().navigate(R.id.action_moviesFragment_to_movieDetailsFragment)
+            }
+        }
+    }
+
+    private fun setupMoviesAdapter() {
+        moviesAdapter.expressionOnCreateViewHolder = { viewGroup ->
+            ItemMediaBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        }
+
+        moviesAdapter.expressionOnBindViewHolder = { movie, viewBinding ->
+            val view = viewBinding as ItemMediaBinding
+            view.root.setOnClickListener {
+                findNavController().navigate(R.id.action_moviesFragment_to_movieDetailsFragment)
+            }
+        }
+    }
+
+    private fun setupMoviesRecyclerView() {
+        binding.rvMovies.apply {
+            adapter = moviesAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    private fun setupTopRatedMoviesRecylerView() {
+        binding.rvTopRatedMovies.apply {
+            adapter = topRatedMoviesAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         }
     }
 }
